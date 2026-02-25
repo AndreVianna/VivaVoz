040abdf4258aafe655fcf9a0c1c61c9d12a9f866
Andre Vianna <andre.vianna@rossvideo.com>
2026-02-25 12:57:29 -0500

    Added migration

---
+++ source/VivaVoz/Migrations/20260225175010_RemoveExportFormatFromSettings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VivaVoz.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExportFormatFromSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExportFormat",
+                table: "Settings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExportFormat",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "MP3");
+        }
+    }
+}
